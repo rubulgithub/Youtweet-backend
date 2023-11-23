70274d9afe02d059a87995a3b5a32be85d599839
rubulgithub <114800261+rubulgithub@users.noreply.github.com>
2023-11-23 21:45:54 +0530

    all registr controler

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,16 +18,26 @@ const registerUser = asyncHandler(async (req, res) => {
 
   //check user already exists: username ,email
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ email }, { username }],
   });
   if (existedUser) {
-    throw new ApiError(409, "User with email or username already existed");
+    throw new ApiError(409, "User with email or username already exists");
   }
 
+  // console.log(req.files);
   //check for images,check for images
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.avatar[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
