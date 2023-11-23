f55eeeacfabbb9190e6e7baca41c1f8abb66ad6c
rubulgithub <114800261+rubulgithub@users.noreply.github.com>
2023-11-23 14:53:40 +0530

    changes in routes

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import connectionDB from "./db/index.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectionDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
