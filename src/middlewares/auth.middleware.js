54d536851c68255a5b2e29f491162db9300ac4dc
Rubul Islam <114800261+rubulgithub@users.noreply.github.com>
2024-04-25 21:53:14 +0530

    auth

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,6 +8,7 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     const token =
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
+    console.log("Token", token);
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
